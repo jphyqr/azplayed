1d685066957a6d64ef6c0373e9f0fb02d55de631
John Hashem <jphyqr@gmail.com>
2019-06-10 14:15:07 -0600

    initial commit

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+
+
+
+
+class  App extends Component {
+
+  render(){
+
+    return (
+      <div className="App">
+  <h1>Azplayed</h1>
+  
+      </div>
+    );
+
+  }
+
+}
+
+export default App;
